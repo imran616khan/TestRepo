dfdb753f7a3febb8e90b5308c7d88c0d0bf8bdc7
slinger <imran.khan@dish.com>
2021-06-12 00:19:45 +0530

    updated webdriver

--- src/test/java/TestPackage/TestClass.java
+++ src/test/java/TestPackage/TestClass.java
@@ -1,9 +1,15 @@
 package TestPackage;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hi world");
+		System.setProperty("webdriver.chrome.driver", "./Executables/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
 	}
 
 }
